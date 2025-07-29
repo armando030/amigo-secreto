0c2984743ee0404e931f6be4f45fe0b8639ee939
Andre <andre05117@gmail.com>
2025-07-28 20:32:40 -0500

    primera versión

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Crea un array para almacenar los nombres
+
+let listaNombres = [];
+let nombre = "";
+let amigoSeleccionado = "";
+
+// función para agregar nombres al array
+function agregarAmigo(nombre) {
+    document.getElementById("resultado").innerHTML = "";  
+    nombre = document.getElementById("amigo").value.trim();
+    if (nombre != "") {
+        listaNombres.push(nombre);
+        // Limpiar la lista antes de agregar nuevos nombres
+        document.getElementById("listaAmigos").innerHTML = "";
+        //document.getElementById("listaAmigos").innerHTML += `<li>${nombre}</li>`;
+        console.log(`Nombre "${nombre}" agregado.`);
+        // Limpiar el campo de entrada
+        document.getElementById("amigo").value = "";
+        for (let i=0 ; i < listaNombres.length; i++) {
+            // i: posición del elemento en el array
+            document.getElementById("listaAmigos").innerHTML += `<li>${listaNombres[i]}</li>`;
+        }
+    } else {
+        alert("Por favor, ingresa un nombre válido.");
+    }
+    console.log("Lista de nombres:", listaNombres);
+}
+
+// función para seleccionar un amigo al azar
+
+function sortearAmigo() {
+    if (listaNombres.length < 2) {
+        alert("Por favor, agrega al menos dos amigos para realizar el sorteo.");
+        return;
+    }
+    // Seleccionar un amigo al azar
+    const posicionAleatoria = Math.floor(Math.random() * listaNombres.length);
+    amigoSeleccionado = listaNombres[posicionAleatoria];
+    // Mostrar el amigo seleccionado
+    document.getElementById("resultado").innerHTML = `Amigo seleccionado: ${amigoSeleccionado}`;
+    // Limpiar la lista de amigos
+    listaNombres = [];
+    document.getElementById("listaAmigos").innerHTML = "";
+} 
